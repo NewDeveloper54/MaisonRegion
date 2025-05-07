9f010ae579f0e851c4e1379c5bf383dd700ad2e5
Amar AMEDJKANE <amedjkane.amar02@gmail.com>
2025-05-07 10:51:25 +0200

    changes ins MaisonRegional

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,11 +1,6 @@
 import React, { useState, useEffect, use } from "react";
 import "./Main.css";
-import Notaire from "../assets/logoNotaire.png";
 import mp44 from "../assets/mp44.png";
-import donation from "../assets/acteDonation.png";
-import immobilierNotaire from "../assets/immobilierNotaire.png";
-import successionNotaire from "../assets/successionNotaire.png";
-import entreprise from "../assets/entreprise.png";
 import GES from "../assets/GES.svg";
 import acces from "../assets/acces2.png";
 import telephoner from "../assets/appel.png";
@@ -103,16 +98,27 @@ const Main = () => {
 
         <div className="elements">
 
-
           <div className={`priority ${sectionTwoShowed ? "show" : ""}`}>
 
           
-          <a href="https://www.grandest.fr/decouvrir-richesses/offre-sportive/">
-            <div className="item one">
-            { /* <img src={acces} height="150" width="160" alt="" />   */ }
-              <h1>OFFRE SPORTIVE</h1>
-            </div>
-          </a>
+          <div class="group">
+  <svg viewBox="0 0 24 24" aria-hidden="true" class="search-icon">
+    <g>
+      <path
+        d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"
+      ></path>
+    </g>
+  </svg>
+
+  <input
+    id="query"
+    class="input"
+    type="search"
+    placeholder="Search..."
+    name="searchbar"
+  />
+</div>
+              <h1 style={{color:"white"}}>MAISON REGIONALE DES SPORTS : 13 rue Jean Moulin 54510 TOMBLAINE</h1>
 
           </div>
 
@@ -126,7 +132,460 @@ const Main = () => {
           >
             <div className="item two">
               <img src={telephoner} alt="" height="100" width="100" />
-              <h1 className="angled-shadow">CONTACT</h1>
+              <h1 className="angled-shadow">REGION GRAND EST</h1>
+            </div>
+          </a>
+
+          <a href="https://www.grandest.fr/wp-content/uploads/2025/01/tomblaine.pdf"    className={`hidden ${show3 ? "show2" : ""}`}>
+            <div className="item three">
+              <img src={acces} alt="" height="140" width="150" />
+              <h1>ASPTT COMITE REGIONAL GRAND EST</h1>
+            </div>
+          </a>
+
+          <a
+            href="https://www.grandest.fr/fonctionnement-de-la-region/les-maisons-de-la-region/#flex_image__video"
+            rel="noopener noreferrer"
+            className={`hidden ${show4 ? "show2" : ""}`}
+          >
+            <div className="item four">
+              <img src={domicile} alt="" height="100" width="100" />
+              <h1 className="angled-shadow">ATHLETISME LIGUE REGIONALE GRAND EST</h1>
+            </div>
+          </a>
+
+
+          <a
+            href="https://www.grandest.fr/contact/"
+            rel="noopener noreferrer"
+            className={`hidden ${show2 ? "show2" : ""}`}
+          >
+            <div className="item two">
+              <img src={telephoner} alt="" height="100" width="100" />
+              <h1 className="angled-shadow">AVIRON LIGUE REGIONALE GRAND EST</h1>
+            </div>
+          </a>
+
+          <a href="https://www.grandest.fr/wp-content/uploads/2025/01/tomblaine.pdf"    className={`hidden ${show3 ? "show2" : ""}`}>
+            <div className="item three">
+              <img src={acces} alt="" height="140" width="150" />
+              <h1>BADMINTON LIGUE REGIONALE GRAND EST</h1>
+            </div>
+          </a>
+
+          <a
+            href="https://www.grandest.fr/fonctionnement-de-la-region/les-maisons-de-la-region/#flex_image__video"
+            rel="noopener noreferrer"
+            className={`hidden ${show4 ? "show2" : ""}`}
+          >
+            <div className="item four">
+              <img src={domicile} alt="" height="100" width="100" />
+              <h1 className="angled-shadow">BASKET LIGUE REGIONALE GRAND EST</h1>
+            </div>
+          </a>
+
+
+
+          <a
+            href="https://www.grandest.fr/contact/"
+            rel="noopener noreferrer"
+            className={`hidden ${show2 ? "show2" : ""}`}
+          >
+            <div className="item two">
+              <img src={telephoner} alt="" height="100" width="100" />
+              <h1 className="angled-shadow">CANOE KAYAK COMITE REGIONAL GRAND EST</h1>
+            </div>
+          </a>
+
+          <a href="https://www.grandest.fr/wp-content/uploads/2025/01/tomblaine.pdf"    className={`hidden ${show3 ? "show2" : ""}`}>
+            <div className="item three">
+              <img src={acces} alt="" height="140" width="150" />
+              <h1>CRESS GRAND EST</h1>
+            </div>
+          </a>
+
+          <a
+            href="https://www.grandest.fr/fonctionnement-de-la-region/les-maisons-de-la-region/#flex_image__video"
+            rel="noopener noreferrer"
+            className={`hidden ${show4 ? "show2" : ""}`}
+          >
+            <div className="item four">
+              <img src={domicile} alt="" height="100" width="100" />
+              <h1 className="angled-shadow">CROS GRAND EST</h1>
+            </div>
+          </a>
+
+
+
+
+          <a
+            href="https://www.grandest.fr/contact/"
+            rel="noopener noreferrer"
+            className={`hidden ${show2 ? "show2" : ""}`}
+          >
+            <div className="item two">
+              <img src={telephoner} alt="" height="100" width="100" />
+              <h1 className="angled-shadow">CYCLISME COMITE REGIONAL GRAND EST</h1>
+            </div>
+          </a>
+
+          <a href="https://www.grandest.fr/wp-content/uploads/2025/01/tomblaine.pdf"    className={`hidden ${show3 ? "show2" : ""}`}>
+            <div className="item three">
+              <img src={acces} alt="" height="140" width="150" />
+              <h1>EQUITATION COMITE REGIONAL GRNAD EST</h1>
+            </div>
+          </a>
+
+          <a
+            href="https://www.grandest.fr/fonctionnement-de-la-region/les-maisons-de-la-region/#flex_image__video"
+            rel="noopener noreferrer"
+            className={`hidden ${show4 ? "show2" : ""}`}
+          >
+            <div className="item four">
+              <img src={domicile} alt="" height="100" width="100" />
+              <h1 className="angled-shadow">EPGV COMITE REGIONAL GRAND EST </h1>
+            </div>
+          </a>
+
+
+
+
+          <a
+            href="https://www.grandest.fr/contact/"
+            rel="noopener noreferrer"
+            className={`hidden ${show2 ? "show2" : ""}`}
+          >
+            <div className="item two">
+              <img src={telephoner} alt="" height="100" width="100" />
+              <h1 className="angled-shadow">EPGV COMITE DEPARTEMENTAL MEURTHE ET MOSELLE</h1>
+            </div>
+          </a>
+
+          <a href="https://www.grandest.fr/wp-content/uploads/2025/01/tomblaine.pdf"    className={`hidden ${show3 ? "show2" : ""}`}>
+            <div className="item three">
+              <img src={acces} alt="" height="140" width="150" />
+              <h1>ESCRIME LIGUE REGIONALE GRAND EST</h1>
+            </div>
+          </a>
+
+          <a
+            href="https://www.grandest.fr/fonctionnement-de-la-region/les-maisons-de-la-region/#flex_image__video"
+            rel="noopener noreferrer"
+            className={`hidden ${show4 ? "show2" : ""}`}
+          >
+            <div className="item four">
+              <img src={domicile} alt="" height="100" width="100" />
+              <h1 className="angled-shadow">FNMNS</h1>
+            </div>
+          </a>
+
+
+
+
+          <a
+            href="https://www.grandest.fr/contact/"
+            rel="noopener noreferrer"
+            className={`hidden ${show2 ? "show2" : ""}`}
+          >
+            <div className="item two">
+              <img src={telephoner} alt="" height="100" width="100" />
+              <h1 className="angled-shadow">FNMNS CNF</h1>
+            </div>
+          </a>
+
+          <a href="https://www.grandest.fr/wp-content/uploads/2025/01/tomblaine.pdf"    className={`hidden ${show3 ? "show2" : ""}`}>
+            <div className="item three">
+              <img src={acces} alt="" height="140" width="150" />
+              <h1>GOLF LIGUE REGIONALE GRAND EST</h1>
+            </div>
+          </a>
+
+          <a
+            href="https://www.grandest.fr/fonctionnement-de-la-region/les-maisons-de-la-region/#flex_image__video"
+            rel="noopener noreferrer"
+            className={`hidden ${show4 ? "show2" : ""}`}
+          >
+            <div className="item four">
+              <img src={domicile} alt="" height="100" width="100" />
+              <h1 className="angled-shadow"> GYMNASTIQUE COMITE REGIONAL GRAND EST</h1>
+            </div>
+          </a>
+
+
+
+
+
+          <a
+            href="https://www.grandest.fr/contact/"
+            rel="noopener noreferrer"
+            className={`hidden ${show2 ? "show2" : ""}`}
+          >
+            <div className="item two">
+              <img src={telephoner} alt="" height="100" width="100" />
+              <h1 className="angled-shadow">HANDBALL LIGUE REGIONALE GRAND EST</h1>
+            </div>
+          </a>
+
+          <a href="https://www.grandest.fr/wp-content/uploads/2025/01/tomblaine.pdf"    className={`hidden ${show3 ? "show2" : ""}`}>
+            <div className="item three">
+              <img src={acces} alt="" height="140" width="150" />
+              <h1>HANDBALL COMITE DEPARTEMENTAL MEURTHE ET MOSELLE</h1>
+            </div>
+          </a>
+
+          <a
+            href="https://www.grandest.fr/fonctionnement-de-la-region/les-maisons-de-la-region/#flex_image__video"
+            rel="noopener noreferrer"
+            className={`hidden ${show4 ? "show2" : ""}`}
+          >
+            <div className="item four">
+              <img src={domicile} alt="" height="100" width="100" />
+              <h1 className="angled-shadow">HANDISPORT COMITE REGIONAL GRAND EST</h1>
+            </div>
+          </a>
+
+
+
+
+
+
+          <a
+            href="https://www.grandest.fr/contact/"
+            rel="noopener noreferrer"
+            className={`hidden ${show2 ? "show2" : ""}`}
+          >
+            <div className="item two">
+              <img src={telephoner} alt="" height="100" width="100" />
+              <h1 className="angled-shadow">HANDISPORT COMITE DEPARTEMENTAL MEURTHE ET MOSELLE</h1>
+            </div>
+          </a>
+
+          <a href="https://www.grandest.fr/wp-content/uploads/2025/01/tomblaine.pdf"    className={`hidden ${show3 ? "show2" : ""}`}>
+            <div className="item three">
+              <img src={acces} alt="" height="140" width="150" />
+              <h1>JUDO LIGUE REGIONALE GRAND EST</h1>
+            </div>
+          </a>
+
+          <a
+            href="https://www.grandest.fr/fonctionnement-de-la-region/les-maisons-de-la-region/#flex_image__video"
+            rel="noopener noreferrer"
+            className={`hidden ${show4 ? "show2" : ""}`}
+          >
+            <div className="item four">
+              <img src={domicile} alt="" height="100" width="100" />
+              <h1 className="angled-shadow">JUDO COMITE DEPARTEMENTAL MEURTHE ET MOSELLE</h1>
+            </div>
+          </a>
+
+
+
+
+
+          <a
+            href="https://www.grandest.fr/contact/"
+            rel="noopener noreferrer"
+            className={`hidden ${show2 ? "show2" : ""}`}
+          >
+            <div className="item two">
+              <img src={telephoner} alt="" height="100" width="100" />
+              <h1 className="angled-shadow">LORRAINE MOUVEMENT ASSOCIATIF</h1>
+            </div>
+          </a>
+
+          <a href="https://www.grandest.fr/wp-content/uploads/2025/01/tomblaine.pdf"    className={`hidden ${show3 ? "show2" : ""}`}>
+            <div className="item three">
+              <img src={acces} alt="" height="140" width="150" />
+              <h1>MOTOCYCLISME LIGUE REGIONALE GRAND EST</h1>
+            </div>
+          </a>
+
+          <a
+            href="https://www.grandest.fr/fonctionnement-de-la-region/les-maisons-de-la-region/#flex_image__video"
+            rel="noopener noreferrer"
+            className={`hidden ${show4 ? "show2" : ""}`}
+          >
+            <div className="item four">
+              <img src={domicile} alt="" height="100" width="100" />
+              <h1 className="angled-shadow">NATATION LIGUE REGIONALE GRAND EST</h1>
+            </div>
+          </a>
+
+
+
+
+
+          <a
+            href="https://www.grandest.fr/contact/"
+            rel="noopener noreferrer"
+            className={`hidden ${show2 ? "show2" : ""}`}
+          >
+            <div className="item two">
+              <img src={telephoner} alt="" height="100" width="100" />
+              <h1 className="angled-shadow">COMITE PARALYMPIQUE ET SPORTIF Français</h1>
+            </div>
+          </a>
+
+          <a href="https://www.grandest.fr/wp-content/uploads/2025/01/tomblaine.pdf"    className={`hidden ${show3 ? "show2" : ""}`}>
+            <div className="item three">
+              <img src={acces} alt="" height="140" width="150" />
+              <h1>PETANQUE COMITE DEPARTEMENTAL MEURTHE ET MOSELLE</h1>
+            </div>
+          </a>
+
+          <a
+            href="https://www.grandest.fr/fonctionnement-de-la-region/les-maisons-de-la-region/#flex_image__video"
+            rel="noopener noreferrer"
+            className={`hidden ${show4 ? "show2" : ""}`}
+          >
+            <div className="item four">
+              <img src={domicile} alt="" height="100" width="100" />
+              <h1 className="angled-shadow">RANDONNEE PEDESTRE COMITE REGIONAL GRAND EST</h1>
+            </div>
+          </a>
+
+
+
+
+
+          <a
+            href="https://www.grandest.fr/contact/"
+            rel="noopener noreferrer"
+            className={`hidden ${show2 ? "show2" : ""}`}
+          >
+            <div className="item two">
+              <img src={telephoner} alt="" height="100" width="100" />
+              <h1 className="angled-shadow">RANDONNEE PEDESTRE COMITE DEPARTEMENTAL MEURTHE ET MOSELLE</h1>
+            </div>
+          </a>
+
+          <a href="https://www.grandest.fr/wp-content/uploads/2025/01/tomblaine.pdf"    className={`hidden ${show3 ? "show2" : ""}`}>
+            <div className="item three">
+              <img src={acces} alt="" height="140" width="150" />
+              <h1>RUGBY LIGUE REGIONALE GRAND EST</h1>
+            </div>
+          </a>
+
+          <a
+            href="https://www.grandest.fr/fonctionnement-de-la-region/les-maisons-de-la-region/#flex_image__video"
+            rel="noopener noreferrer"
+            className={`hidden ${show4 ? "show2" : ""}`}
+          >
+            <div className="item four">
+              <img src={domicile} alt="" height="100" width="100" />
+              <h1 className="angled-shadow">SPELEOLOGIE LIGUE REGIONALE GRAND EST </h1>
+            </div>
+          </a>
+
+
+
+
+
+          <a
+            href="https://www.grandest.fr/contact/"
+            rel="noopener noreferrer"
+            className={`hidden ${show2 ? "show2" : ""}`}
+          >
+            <div className="item two">
+              <img src={telephoner} alt="" height="100" width="100" />
+              <h1 className="angled-shadow">SPORT ADAPTE LIGUE REGIONALE GRAND EST</h1>
+            </div>
+          </a>
+
+          <a href="https://www.grandest.fr/wp-content/uploads/2025/01/tomblaine.pdf"    className={`hidden ${show3 ? "show2" : ""}`}>
+            <div className="item three">
+              <img src={acces} alt="" height="140" width="150" />
+              <h1>SPORT AUTOMOBILE LIGUE REGIONALE GRAND EST</h1>
+            </div>
+          </a>
+
+          <a
+            href="https://www.grandest.fr/fonctionnement-de-la-region/les-maisons-de-la-region/#flex_image__video"
+            rel="noopener noreferrer"
+            className={`hidden ${show4 ? "show2" : ""}`}
+          >
+            <div className="item four">
+              <img src={domicile} alt="" height="100" width="100" />
+              <h1 className="angled-shadow">SPORT POUR TOUS COMITE REGIONAL GRAND EST</h1>
+            </div>
+          </a>
+
+
+
+
+          <a
+            href="https://www.grandest.fr/contact/"
+            rel="noopener noreferrer"
+            className={`hidden ${show2 ? "show2" : ""}`}
+          >
+            <div className="item two">
+              <img src={telephoner} alt="" height="100" width="100" />
+              <h1 className="angled-shadow">TAEKWONDO LIGUE REGIONALE GRAND EST</h1>
+            </div>
+          </a>
+
+          <a href="https://www.grandest.fr/wp-content/uploads/2025/01/tomblaine.pdf"    className={`hidden ${show3 ? "show2" : ""}`}>
+            <div className="item three">
+              <img src={acces} alt="" height="140" width="150" />
+              <h1>TENNIS LIGUE REGIONALE GRAND EST </h1>
+            </div>
+          </a>
+
+          <a
+            href="https://www.grandest.fr/fonctionnement-de-la-region/les-maisons-de-la-region/#flex_image__video"
+            rel="noopener noreferrer"
+            className={`hidden ${show4 ? "show2" : ""}`}
+          >
+            <div className="item four">
+              <img src={domicile} alt="" height="100" width="100" />
+              <h1 className="angled-shadow">TENNIS DE TABLE LIGUE REGIONALE GRAND EST</h1>
+            </div>
+          </a>
+
+
+
+
+
+          <a
+            href="https://www.grandest.fr/contact/"
+            rel="noopener noreferrer"
+            className={`hidden ${show2 ? "show2" : ""}`}
+          >
+            <div className="item two">
+              <img src={telephoner} alt="" height="100" width="100" />
+              <h1 className="angled-shadow">TRIATHLON LIGUE REGIONALE GRAND EST</h1>
+            </div>
+          </a>
+
+          <a href="https://www.grandest.fr/wp-content/uploads/2025/01/tomblaine.pdf"    className={`hidden ${show3 ? "show2" : ""}`}>
+            <div className="item three">
+              <img src={acces} alt="" height="140" width="150" />
+              <h1>UNSS</h1>
+            </div>
+          </a>
+
+          <a
+            href="https://www.grandest.fr/fonctionnement-de-la-region/les-maisons-de-la-region/#flex_image__video"
+            rel="noopener noreferrer"
+            className={`hidden ${show4 ? "show2" : ""}`}
+          >
+            <div className="item four">
+              <img src={domicile} alt="" height="100" width="100" />
+              <h1 className="angled-shadow">VOLLEY BALL LIGUE REGIONALE GRAND EST</h1>
+            </div>
+          </a>
+
+
+
+
+          <a
+            href="https://www.grandest.fr/contact/"
+            rel="noopener noreferrer"
+            className={`hidden ${show2 ? "show2" : ""}`}
+          >
+            <div className="item two">
+              <img src={telephoner} alt="" height="100" width="100" />
+              <h1 className="angled-shadow">UGSEL 54</h1>
             </div>
           </a>
 
@@ -147,6 +606,11 @@ const Main = () => {
               <h1 className="angled-shadow">MES MAISONS DE LA REGION</h1>
             </div>
           </a>
+
+
+
+
+
           </div>
 
         </div>
